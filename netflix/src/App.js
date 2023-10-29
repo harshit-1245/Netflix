6c6e2df7d0df43cdf46531e1dc1f9c7912337612
harshit-506 <122781586+harshit-506@users.noreply.github.com>
2023-10-29 23:48:12 +0530

    90% Work done

---
+++ netflix/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import './App.scss';
+import Header from './components/Header.js';
+import HomeBanner from "./components/HomeBanner";
+import Login from "./components/Login";
+import Banner from "./components/Banner";
+import List from "./components/List";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <React.Fragment>
+              <Header/>
+              <HomeBanner/>
+            </React.Fragment>
+          }/>
+          <Route path="/login" element={
+            <React.Fragment>
+              <Header/>
+              {/* this is helpful for change dynamically */}
+              <Login/> 
+            </React.Fragment>
+          }/>
+           <Route path="/dashboard" element={
+            <React.Fragment>
+              <Header/>
+              <Banner/>
+              <List/>
+            </React.Fragment>
+          }/>
+          <Route path="/register" element={
+            <React.Fragment>
+              <Header/>
+              <Login/>
+            </React.Fragment>
+          }/>
+             </Routes>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ netflix/src/components/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCH_5YBq2feCGFbY7t8A9CYRsQ7NPXQI7o",
+  authDomain: "netflix-5754e.firebaseapp.com",
+  projectId: "netflix-5754e",
+  storageBucket: "netflix-5754e.appspot.com",
+  messagingSenderId: "167832611137",
+  appId: "1:167832611137:web:9bbc86e1f14dd3a371d9c0",
+  measurementId: "G-ME9FY104F6"
+};
+
+// Initialize Firebase
+
